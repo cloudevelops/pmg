27a6459bd936ba98a288f88bcb7c30f1191e990b
Michal Vesely <michal.vesely@cloudevelops.com>
2018-01-11 19:41:13 +0100

    add puppetserver1[1,2].cz2.cloudevelops.lan

--- cmd/root.go
+++ cmd/root.go
@@ -90,7 +90,7 @@ func initConfig() {
 	}
 
 	viper.SetDefault("hook.githome", "/home/git/gogs-repositories")
-	puppetServers := []string{"puppetserver1.cz2.cloudevelops.lan","puppetserver2.cz2.cloudevelops.lan", "puppetserver3.cz2.cloudevelops.lan", "puppetserver4.cz2.cloudevelops.lan", "puppetserver5.cz2.cloudevelops.lan", "puppetserver6.cz2.cloudevelops.lan", "puppetserver7.cz2.cloudevelops.lan", "puppetserver8.cz2.cloudevelops.lan", "puppetserver9.cz2.cloudevelops.lan", "puppetserver10.cz2.cloudevelops.lan", "puppetserver1.cz2.gtflixtv.lan", "puppetserver2.cz2.gtflixtv.lan", "puppetserver3.cz2.gtflixtv.lan", "puppetserver4.cz2.gtflixtv.lan", "puppetserver5.cz2.gtflixtv.lan", "puppetserver6.cz2.gtflixtv.lan"}
+	puppetServers := []string{"puppetserver1.cz2.cloudevelops.lan","puppetserver2.cz2.cloudevelops.lan", "puppetserver3.cz2.cloudevelops.lan", "puppetserver4.cz2.cloudevelops.lan", "puppetserver5.cz2.cloudevelops.lan", "puppetserver6.cz2.cloudevelops.lan", "puppetserver7.cz2.cloudevelops.lan", "puppetserver8.cz2.cloudevelops.lan", "puppetserver9.cz2.cloudevelops.lan", "puppetserver10.cz2.cloudevelops.lan", "puppetserver11.cz2.cloudevelops.lan", "puppetserver12.cz2.cloudevelops.lan", "puppetserver1.cz2.gtflixtv.lan", "puppetserver2.cz2.gtflixtv.lan", "puppetserver3.cz2.gtflixtv.lan", "puppetserver4.cz2.gtflixtv.lan", "puppetserver5.cz2.gtflixtv.lan", "puppetserver6.cz2.gtflixtv.lan"}
 	viper.SetDefault("hook.puppetservers", puppetServers)
 
 	viper.AutomaticEnv() // read in environment variables that match
